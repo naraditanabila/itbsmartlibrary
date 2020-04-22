ab1dcdd3e7fc09f68762fb3773920fd3f510e315
naraditanabila <naraditanabila@gmail.com>
2020-04-22 17:31:19 +0700

    update for deploying

---
+++ app.js
@@ -0,0 +1,32 @@
+const Express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = process.env.PORT||3001
+const app = Express()
+const Pool = require('pg').Pool
+require('dotenv').config()
+
+const db = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  ssl: true
+})
+
+db.connect();
+
+const anggotaRouter = require('./api/anggota')
+const bukuRouter = require('./api/buku')
+const peminjamanRouter = require('./api/peminjaman')
+
+
+app.use(bodyParser())
+app.use(cors())
+
+app.use(anggotaRouter)
+app.use(bukuRouter)
+app.use(peminjamanRouter)
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,44 +1,107 @@
-const Express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const port = process.env.PORT||3001
-const app = Express()
-const Pool = require('pg').Pool
-require('dotenv').config()
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var app = require('./app');
+var debug = require('debug')('demo:server');
+var http = require('http');
 
 var static = require ('node-static')
 var file = new static.Server('./frontend');
 
-const db = new Pool({
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  ssl: true
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3001');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(function(request, response){
+  request.addListener('end', function() {
+    file.serve(request,response);
+  }).resume();
 })
 
-db.connect();
-
-
-/*const app = require('http').createServer(function(request, response){
+/*app.require('http').createServer(function(request, response){
   request.addListener('end', function() {
     file.serve(request,response);
   }).resume();
 })*/
 
-const anggotaRouter = require('./api/anggota')
-const bukuRouter = require('./api/buku')
-const peminjamanRouter = require('./api/peminjaman')
-
-
-app.use(bodyParser())
-app.use(cors())
-
-app.use(anggotaRouter)
-app.use(bukuRouter)
-app.use(peminjamanRouter)
-
-app.listen(port, function() {
+/*app.listen(port, function() {
   console.log(`Server Starts on '${port}'`)
-});
+});*/
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
