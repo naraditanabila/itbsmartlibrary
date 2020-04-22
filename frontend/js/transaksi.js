f89dca52059c7997b28a67e2bc76d26744eaadcb
naraditanabila <naraditanabila@gmail.com>
2020-04-22 15:45:01 +0700

    update frontend

---
+++ frontend/js/transaksi.js
@@ -0,0 +1,16 @@
+const addTransaksi = async() => {
+	const data = await JSON.stringify({
+		id_anggota:document.getElementById("m_id_anggota").value,
+		id_buku:document.getElementById("m_id_buku").value,	
+	})
+	console.log(data);
+	const response = await fetch('https://itbsmartlibrary.herokuapp.com/peminjaman/add',{
+		method:'POST',
+		headers:{
+			'Content-Type':'application/json'
+		},
+		body: data
+	})
+	const json = await response.json();
+	console.log(json);
+}
